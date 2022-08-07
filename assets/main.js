53f5691f595565b567d6bf2ae77afcf8680d6540
Diego_Flores <ldiego.flores98@gmail.com>
2022-08-06 21:51:19 -0700

    Updates

---
+++ assets/main.js
@@ -0,0 +1,42 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UCB6IiFWprIPnJX40m52c0og&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '40881a9b08msh14c799c0eb78b3dp10e811jsn40d090591fe2',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi, options);
+    const data = await response.json();
+    return data;
+}
+
+(async () => {
+    try {
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video =>`
+        <div class="group relative">
+        <div
+          class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+          <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+        </div>
+        <div class="mt-4 flex justify-between">
+          <h3 class="text-sm text-gray-700">
+            <span aria-hidden="true" class="absolute inset-0"></span>
+            ${video.snippet.title}
+          </h3>
+        </div>
+      </div>
+        `).slice(0,4).join('')}
+        `;
+        content.innerHTML = view;
+    } catch (error) {
+        console.log(error);
+    }
+})();
